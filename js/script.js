fd04b9d4fb862c1ef715f902fb0541e55ccc3526
Daniel <DAbrego@alvincollege.edu>
2016-12-27 23:24:24 -0600

    whitespace

--- js/script.js
+++ js/script.js
@@ -1,48 +1,46 @@
 
 function loadData() {
-    var $wikiElem = $('#wikipedia-links');	
-	
-	$wikiElem.empty();
-	$wikiElem.append('<h3>Wikipedia Links:</h3>');
-					
-	var queryStr = $('#query1').val();
-    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + queryStr + '&format=json&callback=?';
-    
-	// set timeout to do something if ajax call to wikipedia fails
-	var wTimeout = setTimeout(function() {
-      $wikiElem.text("failed to get wikipedia resources");
-    }, 8000);
+  var $wikiElem = $('#wikipedia-links');
 
-    $.ajax({
-		 type: "GET", 
-		 url: wikiUrl,
-		 dataType:"jsonP",
-		 success: function(response) {
-			//console.log("response: [" + response + "]");
-			var articleList = response[1];
-			//console.log(articleList.length);
-			for (var i1=0; i1<articleList.length; i1++) {
-				articleStr = articleList[i1];
-				console.log("articleStr : " + articleStr);		 
-				var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-				var openAnchor = '<a href="' + url + '" target="_blank">';
-				$wikiElem.append('<li>' + openAnchor + articleStr + '</a></li>');
-			};
+  $wikiElem.empty();
+  $wikiElem.append('<h3>Wikipedia Links:</h3>');
 
-			// clear timeout if ajax call succeeds
-			clearTimeout(wTimeout);
-		 }
-    });
+  var queryStr = $('#query1').val();
+  var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + queryStr + '&format=json&callback=?';
 
-    return false;
+  // set timeout to do something if ajax call to wikipedia fails
+  var wTimeout = setTimeout(function () {
+    $wikiElem.text("failed to get wikipedia resources");
+  }, 8000);
+
+  $.ajax({
+    type: "GET",
+    url: wikiUrl,
+    dataType: "jsonP",
+    success: function (response) {
+      //console.log("response: [" + response + "]");
+      var articleList = response[1];
+      //console.log(articleList.length);
+      for (var i1 = 0; i1 < articleList.length; i1++) {
+        articleStr = articleList[i1];
+        console.log("articleStr : " + articleStr);
+        var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+        var openAnchor = '<a href="' + url + '" target="_blank">';
+        $wikiElem.append('<li>' + openAnchor + articleStr + '</a></li>');
+      };
+
+      // clear timeout if ajax call succeeds
+      clearTimeout(wTimeout);
+    }
+  });
+
+  return false;
 };
 
-$(document).ready(function(){
-	$('#form-container').submit(loadData);
-	
-	$("#btnRandom").click(function(){
-		window.location.href = 'https://en.wikipedia.org/wiki/Special:Random';
-	});		
+$(document).ready(function () {
+  $('#form-container').submit(loadData);
+
+  $("#btnRandom").click(function () {
+    window.location.href = 'https://en.wikipedia.org/wiki/Special:Random';
+  });
 });
-
-
